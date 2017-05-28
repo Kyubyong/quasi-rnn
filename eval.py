02b1cd31dd54ebb0a3514a6ee652498deb48efe7
Kyubyong Park <longinglove@nate.com>
2017-05-28 19:54:09 +0900

    Add files via upload

--- eval.py
+++ eval.py
@@ -1,8 +1,8 @@
-#-*- coding: utf-8-*-
+# -*- coding: utf-8 -*-
 from __future__ import print_function
-from hyperparams import Hp
+from hyperparams import Hyperparams as hp
 import codecs
-import sugartensor as tf
+import tensorflow as tf
 import numpy as np
 from prepro import *
 from train import Graph
@@ -10,40 +10,38 @@ from nltk.translate.bleu_score import corpus_bleu
 
 def eval(): 
     # Load graph
-    g = Graph(mode="inference"); print("Graph Loaded")
-        
-    with tf.Session() as sess:
-        # Initialize variables
-        tf.sg_init(sess)
-
-        # Restore parameters
+    g = Graph(is_training=False)
+    with tf.Session(graph=g.graph) as sess:
         saver = tf.train.Saver()
-        saver.restore(sess, tf.train.latest_checkpoint('asset/train'))
+        saver.restore(sess, tf.train.latest_checkpoint(hp.logdir))
         print("Restored!")
-        mname = open('asset/train/checkpoint', 'r').read().split('"')[1] # model name
+        mname = open(hp.logdir + '/checkpoint', 'r').read().split('"')[1] # model name
+        print(mname)
         
         # Load data
         X, Sources, Targets = load_test_data()
         char2idx, idx2char = load_vocab()
-        
-        with codecs.open(mname, "w", "utf-8") as fout:
+         
+        with codecs.open(hp.savedir + "/" + mname, "w", "utf-8") as fout:
             list_of_refs, hypotheses = [], []
-            for i in range(len(X) // Hp.batch_size):
-                # Get mini-batches
-                x = X[i*Hp.batch_size: (i+1)*Hp.batch_size] # mini-batch
-                sources = Sources[i*Hp.batch_size: (i+1)*Hp.batch_size]
-                targets = Targets[i*Hp.batch_size: (i+1)*Hp.batch_size]
+            for i in range(len(X) // hp.batch_size):
                 
-                preds_prev = np.zeros((Hp.batch_size, Hp.maxlen), np.int32)
-                preds = np.zeros((Hp.batch_size, Hp.maxlen), np.int32)        
-                for j in range(Hp.maxlen):
+                # Get mini-batches
+                x = X[i*hp.batch_size: (i+1)*hp.batch_size] # mini-batch
+                sources = Sources[i*hp.batch_size: (i+1)*hp.batch_size]
+                targets = Targets[i*hp.batch_size: (i+1)*hp.batch_size]
+                 
+                preds_prev = np.zeros((hp.batch_size, hp.maxlen), np.int32)
+                preds_prev[:, 0] = 2
+                preds = np.zeros((hp.batch_size, hp.maxlen), np.int32)        
+                for j in range(hp.maxlen):
                     # predict next character
-                    outs = sess.run(g.preds, {g.x: x, g.y_src: preds_prev})
+                    outs = sess.run(g.preds, {g.x: x, g.decoder_inputs: preds_prev})
                     # update character sequence
-                    if j < Hp.maxlen - 1:
+                    if j < hp.maxlen - 1:
                         preds_prev[:, j + 1] = outs[:, j]
                     preds[:, j] = outs[:, j]
-                
+                 
                 # Write to file
                 for source, target, pred in zip(sources, targets, preds): # sentence-wise
                     got = "".join(idx2char[idx] for idx in pred).split(u"␃")[0]
@@ -51,18 +49,18 @@ def eval():
                     fout.write("- expected: " + target + "\n")
                     fout.write("- got: " + got + "\n\n")
                     fout.flush()
-                    
+                     
                     # For bleu score
                     ref = target.split()
                     hypothesis = got.split()
-                    if len(ref) > 2:
+                    if len(ref) > 3 and len(hypothesis) > 3:
                         list_of_refs.append([ref])
                         hypotheses.append(hypothesis)
-            
+             
             # Get bleu score
             score = corpus_bleu(list_of_refs, hypotheses)
             fout.write("Bleu Score = " + str(100*score))
-                                            
+                                                
 if __name__ == '__main__':
     eval()
     print("Done")
--- hyperparams.py
+++ hyperparams.py
@@ -1,9 +1,14 @@
-class Hp:
+class Hyperparams:
     '''Hyperparameters'''
     de_train = 'corpora/train.tags.de-en.de'
     en_train = 'corpora/train.tags.de-en.en'
     de_test = 'corpora/IWSLT16.TED.tst2014.de-en.de.xml'
     en_test = 'corpora/IWSLT16.TED.tst2014.de-en.en.xml'
     maxlen = 150 # Maximum sentence length
-    batch_size = 16
+    batch_size = 32
     hidden_units = 320
+    num_blocks = 7
+    num_epochs = 10
+    lr = 0.0001
+    logdir = 'log'
+    savedir = 'ckpt'
