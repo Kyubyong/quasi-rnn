6d70ef9fe549bc4a54d930d8d524dfe5949e8e3f
Kyubyong Park <longinglove@nate.com>
2017-05-28 19:59:44 +0900

    Update hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -1,14 +1,10 @@
-class Hyperparams:
+
+class Hp:
     '''Hyperparameters'''
     de_train = 'corpora/train.tags.de-en.de'
     en_train = 'corpora/train.tags.de-en.en'
     de_test = 'corpora/IWSLT16.TED.tst2014.de-en.de.xml'
     en_test = 'corpora/IWSLT16.TED.tst2014.de-en.en.xml'
     maxlen = 150 # Maximum sentence length
-    batch_size = 32
+    batch_size = 16
     hidden_units = 320
-    num_blocks = 7
-    num_epochs = 10
-    lr = 0.0001
-    logdir = 'log'
-    savedir = 'ckpt'
